2595a4df28d453b75856b2b14bf4118b81461968
Nick Buonincontri <nbuonin@gmail.com>
2021-03-03 16:34:06 -0500

    Enough already, just delete the image directory...

--- react.mk
+++ react.mk
@@ -31,13 +31,11 @@ snapshot: $(JS_SENTINAL)
 
 deploy-stage: $(JS_SENTINAL) 
 	npm run build:stage \
-	&& mkdir dist && mkdir dist/images && cp src/images/* dist/images/. \
 	&& $(INTERMEDIATE_STEPS) \
 	&& $(S3CMD) $(S3_FLAGS) sync --exclude-from='.s3ignore' . s3://$(STAGING_BUCKET)/
 
 deploy-prod: $(JS_SENTINAL) 
 	npm run build:prod \
-	&& mkdir dist && mkdir dist/images && cp src/images/* dist/images/. \
 	&& $(INTERMEDIATE_STEPS) \
 	&& $(S3CMD) $(S3_FLAGS) sync --exclude-from='.s3ignore' . s3://$(PROD_BUCKET)/
 
