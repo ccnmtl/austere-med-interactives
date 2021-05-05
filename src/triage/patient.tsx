473630359fb16d54426b5f32901116f3db9086e5
Nick Buonincontri <nbuonin@gmail.com>
2021-05-05 09:29:32 -0400

    eslint

--- src/triage/patient.tsx
+++ src/triage/patient.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { setTriageSelectionData } from './triage';
 import Nurse from '../images/iStock-155705146.jpg';
 
